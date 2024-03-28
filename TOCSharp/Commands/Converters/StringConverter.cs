4c4c80bddc82544d02a075597d0972b282fcc0cb
Animadoria <me@animadoria.cf>
2024-03-28 22:56:29 +0000

    Fix nullability

--- TOCSharp/Commands/Converters/StringConverter.cs
+++ TOCSharp/Commands/Converters/StringConverter.cs
@@ -2,7 +2,7 @@ using System.Threading.Tasks;
 
 namespace TOCSharp.Commands.Converters
 {
-    public class StringConverter : IArgumentConverter<string>
+    public class StringConverter : IArgumentConverter<string?>
     {
         public Task<string?> ConvertAsync(CommandContext context, string input)
         {
